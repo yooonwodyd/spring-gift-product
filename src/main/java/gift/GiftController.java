1218f7c06a436a06f53ed69869b4ffb467719cb1
yooonwodyd <dbswodyd00@naver.com>
2024-06-25 14:27:26 +0900

    [feat] Controller 내부에 인메모리 데이터 베이스 역할을 하는 giftMap 구현

--- src/main/java/gift/GiftController.java
+++ src/main/java/gift/GiftController.java
@@ -1,2 +1,13 @@
-package gift;public class GiftController {
+package gift;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GiftController {
+	// InMemory 방식으로 저장. ConHashMap 사용
+	private final Map<Long, Gift> giftMap = new ConcurrentHashMap<>();
+
 }
