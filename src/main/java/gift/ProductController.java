79f7da98871571c36da262944bd1d3e387f37a8b
yooonwodyd <dbswodyd00@naver.com>
2024-06-25 14:52:49 +0900

    [feat] 상품 전체 조회, 수정, 삭제 메서드 추가

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -4,7 +4,11 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,19 +16,48 @@ public class ProductController {
 	// InMemory 방식으로 저장. ConHashMap 사용
 	private final Map<Long, Product> productRepository = new ConcurrentHashMap<>();
 
-	// 선물 등록
+	// 상품 등록
 	@PostMapping("/api/product")
 	public ResponseEntity<?> registerProduct(ProductRequestDto productRequestDto) {
-		// Id 중복의 경우 다음과 같은 로직을 추가할 수 있다.
-		// if (giftMap.containsKey(giftRequestDto.id())) {
-		//	return ResponseEntity.badRequest().build();
-		// }
+		if (productRepository.containsKey(productRequestDto.id())) {
+			return ResponseEntity.badRequest().build();
+		}
 		productRepository.put(productRequestDto.id(), productRequestDto.toEntity());
 		return ResponseEntity.ok().build();
 	}
 
-	// 선물 조회
+	// 상품 조회, id가 없으면 not found
+	@GetMapping("/api/product")
 	public ResponseEntity<Product> getProduct(Long id) {
+		if (!productRepository.containsKey(id)) {
+			return ResponseEntity.notFound().build();
+		}
 		return ResponseEntity.ok(productRepository.get(id));
+	}
+
+	// 상품 전체 조회
+	@GetMapping("/api/products")
+	public ResponseEntity<Map<Long, Product>> getProducts() {
+		return ResponseEntity.ok(productRepository);
+	}
+
+	// 상품 수정, id가 없으면 not found
+	@PutMapping("/api/product")
+	public ResponseEntity<?> updateProduct(Long id, ProductRequestDto productRequestDto) {
+		if (!productRepository.containsKey(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		productRepository.put(id, productRequestDto.toEntity());
+		return ResponseEntity.ok().build();
+	}
+
+	// 상품 삭제 id가 없으면 not found
+	@DeleteMapping("/api/product")
+	public ResponseEntity<?> deleteProduct(Long id) {
+		if (!productRepository.containsKey(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		productRepository.remove(id);
+		return ResponseEntity.ok().build();
 	}
 }
