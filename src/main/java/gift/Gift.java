d7098c59cdd262c4c8734f5cad88220fa6569d82
yooonwodyd <dbswodyd00@naver.com>
2024-06-25 14:40:10 +0900

    [refact] 팩토리 메서드 패턴 적용. getter setter 추가.

--- src/main/java/gift/Gift.java
+++ src/main/java/gift/Gift.java
@@ -5,4 +5,48 @@ public class Gift {
 	private String name;
 	private Long price;
 	private String imageUrl;
+
+	//of
+	public static Gift of(Long id, String name, Long price, String imageUrl) {
+		return new Gift(id, name, price, imageUrl);
+	}
+
+	private Gift(Long id, String name, Long price, String imageUrl) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.imageUrl = imageUrl;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getPrice() {
+		return price;
+	}
+
+	public void setPrice(Long price) {
+		this.price = price;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
 }
