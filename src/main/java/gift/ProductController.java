68447e625d83254c0943e5030a4160efe6442519
yooonwodyd <dbswodyd00@naver.com>
2024-06-26 15:13:38 +0900

    [feat] admin 페이지로 이동하는 메서드 추가, RestController -> Controller로 변경

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -4,22 +4,31 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class ProductController {
 	// InMemory 방식으로 저장. ConHashMap 사용
 	private final Map<Long, Product> productRepository = new ConcurrentHashMap<>();
 
+
+	@GetMapping("/admin")
+	public String viewProducts(Model model) {
+		model.addAttribute("products", productRepository.values());
+		return "admin/mainPage";
+	}
+
 	// 상품 등록
 	@PostMapping("/api/product")
+	@ResponseBody
 	public ResponseEntity<?> registerProduct(@RequestBody ProductRequestDto productRequestDto) {
 		if (productRepository.containsKey(productRequestDto.id())) {
 			return ResponseEntity.badRequest().build();
@@ -29,6 +38,7 @@ public class ProductController {
 	}
 
 	// 상품 조회, id가 없으면 not found
+	@ResponseBody
 	@GetMapping("/api/product/{id}")
 	public ResponseEntity<Product> getProduct(@PathVariable Long id) {
 		if (!productRepository.containsKey(id)) {
@@ -39,12 +49,14 @@ public class ProductController {
 
 	// 상품 전체 조회
 	@GetMapping("/api/products")
+	@ResponseBody
 	public ResponseEntity<Map<Long, Product>> getProducts() {
 		return ResponseEntity.ok(productRepository);
 	}
 
 	// 상품 수정, id가 없으면 not found
 	@PutMapping("/api/product/{id}")
+	@ResponseBody
 	public ResponseEntity<?> updateProduct(@PathVariable Long id, @RequestBody ProductRequestDto productRequestDto) {
 		if (!productRepository.containsKey(id)) {
 			return ResponseEntity.notFound().build();
@@ -55,6 +67,7 @@ public class ProductController {
 
 	// 상품 삭제 id가 없으면 not found
 	@DeleteMapping("/api/product/{id}")
+	@ResponseBody
 	public ResponseEntity<?> deleteProduct(@PathVariable Long id) {
 		if (!productRepository.containsKey(id)) {
 			return ResponseEntity.notFound().build();
