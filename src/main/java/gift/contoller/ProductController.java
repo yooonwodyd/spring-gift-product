d2ea80e35f970441ff405d29ca351130c7ef9005
yooonwodyd <dbswodyd00@naver.com>
2024-06-27 15:48:57 +0900

    [feat] 기존 인메모리 데이터 베이스 코드 주석 처리 및 Dao로 교체

--- src/main/java/gift/contoller/ProductController.java
+++ src/main/java/gift/contoller/ProductController.java
@@ -1,8 +1,9 @@
-package gift;
+package gift.contoller;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,15 +15,27 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import gift.dao.ProductDao;
+import gift.domain.Product;
+import gift.dto.ProductRequestDto;
+import gift.dto.ProductResponseDto;
+
 @Controller
+
 public class ProductController {
 	// InMemory 방식으로 저장. ConHashMap 사용
-	private final Map<Long, Product> productRepository = new ConcurrentHashMap<>();
+	// private final Map<Long, Product> productRepository = new ConcurrentHashMap<>();
+	@Autowired
+	private ProductDao productDAO;
 
 
 	@GetMapping("/admin")
 	public String viewProducts(Model model) {
-		model.addAttribute("products", productRepository.values());
+		// model.addAttribute("products", productRepository.values());
+		// productDAO.findAll() -> List<ProductResponseDto>, stream() -> Stream<ProductResponseDto>
+
+		model.addAttribute("products", productDAO.findAll().stream().map(ProductResponseDto::from).toList());
+		System.out.println(productDAO.findAll().stream().map(ProductResponseDto::from).toList());
 		return "admin/mainPage";
 	}
 
@@ -30,38 +43,35 @@ public class ProductController {
 	@PostMapping("/api/product")
 	@ResponseBody
 	public ResponseEntity<?> registerProduct(@RequestBody ProductRequestDto productRequestDto) {
-		if (productRepository.containsKey(productRequestDto.id())) {
-			return ResponseEntity.badRequest().build();
-		}
-		productRepository.put(productRequestDto.id(), productRequestDto.toEntity());
+		productDAO.save(productRequestDto.toEntity());
+		// if (productRepository.containsKey(productRequestDto.id())) {
+		// 	return ResponseEntity.badRequest().build();
+		// }
+		// productRepository.put(productRequestDto.id(), productRequestDto.toEntity());
 		return ResponseEntity.ok().build();
 	}
 
 	// 상품 조회, id가 없으면 not found
 	@ResponseBody
 	@GetMapping("/api/product/{id}")
-	public ResponseEntity<Product> getProduct(@PathVariable Long id) {
-		if (!productRepository.containsKey(id)) {
-			return ResponseEntity.notFound().build();
-		}
-		return ResponseEntity.ok(productRepository.get(id));
-	}
+	public ResponseEntity<ProductResponseDto> getProduct(@PathVariable Long id) {
 
-	// 상품 전체 조회
-	@GetMapping("/api/products")
-	@ResponseBody
-	public ResponseEntity<Map<Long, Product>> getProducts() {
-		return ResponseEntity.ok(productRepository);
+		Product product = productDAO.findById(id);
+		// if (!productRepository.containsKey(id)) {
+		// 	return ResponseEntity.notFound().build();
+		// }
+		return ResponseEntity.ok(ProductResponseDto.from(product));
 	}
 
 	// 상품 수정, id가 없으면 not found
 	@PutMapping("/api/product/{id}")
 	@ResponseBody
 	public ResponseEntity<?> updateProduct(@PathVariable Long id, @RequestBody ProductRequestDto productRequestDto) {
-		if (!productRepository.containsKey(id)) {
-			return ResponseEntity.notFound().build();
-		}
-		productRepository.put(id, productRequestDto.toEntity());
+		// if (!productRepository.containsKey(id)) {
+		// 	return ResponseEntity.notFound().build();
+		// }
+		// productRepository.put(id, productRequestDto.toEntity());
+		productDAO.update(productRequestDto.toEntity());
 		return ResponseEntity.ok().build();
 	}
 
@@ -69,10 +79,11 @@ public class ProductController {
 	@DeleteMapping("/api/product/{id}")
 	@ResponseBody
 	public ResponseEntity<?> deleteProduct(@PathVariable Long id) {
-		if (!productRepository.containsKey(id)) {
-			return ResponseEntity.notFound().build();
-		}
-		productRepository.remove(id);
+		// if (!productRepository.containsKey(id)) {
+		// 	return ResponseEntity.notFound().build();
+		// }
+		// productRepository.remove(id);
+		productDAO.deleteById(id);
 		return ResponseEntity.ok().build();
 	}
 
